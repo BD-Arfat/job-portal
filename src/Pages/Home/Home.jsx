d840663e8df7efd335b29f0c3cbda9d36bd8692f
BD-Arfat <arfat1islam5162@gmail.com>
2024-12-18 21:45:17 +0600

    joCard showe hoem

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import JobsAllCards from "./JobsAllCards/JobsAllCards";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <JobsAllCards></JobsAllCards>
     </div>
   );
 };
